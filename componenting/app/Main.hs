8a81f4bbfc5d5dab147ef5be49ff2fdc82f5e8fa
Gustavo Bicalho <gusbicalho@gmail.com>
2020-06-07 22:17:47 +0000

    remove some unused stuff, set settings

--- componenting/app/Main.hs
+++
@@ -1,6 +0,0 @@
-module Main where
-
-import qualified Componenting
-
-main :: IO ()
-main = Componenting.start
--- componenting/src/Componenting.hs
+++ componenting/src/Componenting.hs
@@ -1,8 +1,3 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-module Componenting
-  ( start
-  ) where
-
-start :: IO ()
-start = putStrLn "Running app"
+module Componenting where
